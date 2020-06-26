157cc6aa4a2a0335a72c9bd5027d2c592b4e0beb
m-sabihul-hasan <54771392+m-sabihul-hasan@users.noreply.github.com>
2020-06-26 18:23:49 +0500

    Update ALU_Control.v

--- Task 1/ALU_Control.v
+++ Task 1/ALU_Control.v
@@ -18,16 +18,16 @@ module ALU_Control
 				begin
 				case(Funct)
 				4'b0000:
-					Operation = 4'b0010;
+					Operation = 4'b0010;	//addition
 					
 				4'b1000:
-					Operation = 4'b0110;
+					Operation = 4'b0110;	//subtraction
 					
 				4'b0111:
-					Operation = 4'b0000;
+					Operation = 4'b0000;	//AND
 					
 				4'b0110:
-					Operation = 4'b0001;
+					Operation = 4'b0001;	//OR
 				endcase
 				end
 		endcase
