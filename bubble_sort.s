b4beb5615d1d3f9d80b428f60768afe9336dcc6b
m-sabihul-hasan <54771392+m-sabihul-hasan@users.noreply.github.com>
2020-06-26 12:09:57 +0500

    Create bubble_sort.s

---
+++ bubble_sort.s
@@ -0,0 +1,45 @@
+#setting up len 3 array for sorting
+addi x5, x0 ,3
+sw x5, 0x100(x0)
+addi x5, x0 ,2
+sw x5, 0x104(x0)
+addi x5, x0 ,10
+sw x5, 0x108(x0)
+
+
+addi x10, x0, 0x100 #a pointer
+addi x11, x0, 3 #len
+#jal x1, bubble
+#beq x0, x0, FinalExit
+
+bubble: bne x10, x0, else
+bne x11, x0, else
+beq x0, x0, FinalExit
+#jalr x0, 0(x1)
+
+
+else: addi x18, x0, 0 #i  
+Loop1: beq x18, x11, Exit1
+	add x19, x0, x18 #j = i
+    Loop2:
+    	beq x19, x11, Exit2
+		slli x5, x18, 2 #calculate offset of i
+    	slli x6, x19, 2 #and j
+    	add x5, x5, x10
+        add x6, x6, x10
+        lw x28, 0(x5) #val of a[i]
+        lw x29, 0(x6) #val of a[j]
+        bge x28, x29, loop2_cont #if not a[i] >= [j]
+        add x30, x0, x28 #temp = a[i]
+        add x28, x0, x29 #a[i] = a[j]
+        add x29, x0, x30 #a[j] = temp
+        sw x28, 0(x5)
+        sw x29, 0(x6)
+        loop2_cont: addi x19, x19, 1
+        beq x0, x0, Loop2
+    Exit2: addi x18, x18, 1
+    beq x0, x0, Loop1
+
+Exit1: beq x0, x0, FinalExit
+
+FinalExit: nop
