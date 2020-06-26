b770cad1efd21d90cbac1b0f30342cab6a6f16b4
m-sabihul-hasan <54771392+m-sabihul-hasan@users.noreply.github.com>
2020-06-26 21:01:41 +0500

    Update bubble_sort.s

--- bubble_sort.s
+++ bubble_sort.s
@@ -1,13 +1,13 @@
 #setting up len 3 array for sorting
 addi x5, x0 ,3
-sw x5, 0x100(x0)
+sd x5, 4(x0)
 addi x5, x0 ,2
-sw x5, 0x104(x0)
+sd x5, 12(x0)
 addi x5, x0 ,10
-sw x5, 0x108(x0)
+sd x5, 20(x0)
 
 
-addi x10, x0, 0x100 #a pointer
+addi x10, x0, 4 #a pointer
 addi x11, x0, 3 #len
 
 bubble: bne x10, x0, else
@@ -20,18 +20,18 @@ Loop1: beq x18, x11, Exit1
 	add x19, x0, x18 #j = i
     Loop2:
     	beq x19, x11, Exit2
-		slli x5, x18, 2 #calculate offset of i
-    	slli x6, x19, 2 #and j
+		slli x5, x18, 3 #calculate offset of i
+    	slli x6, x19, 3 #and j
     	add x5, x5, x10
         add x6, x6, x10
-        lw x28, 0(x5) #val of a[i]
-        lw x29, 0(x6) #val of a[j]
+        ld x28, 0(x5) #val of a[i]
+        ld x29, 0(x6) #val of a[j]
         bge x28, x29, loop2_cont #if not a[i] >= [j]
         add x30, x0, x28 #temp = a[i]
         add x28, x0, x29 #a[i] = a[j]
         add x29, x0, x30 #a[j] = temp
-        sw x28, 0(x5)
-        sw x29, 0(x6)
+        sd x28, 0(x5)
+        sd x29, 0(x6)
         loop2_cont: addi x19, x19, 1
         beq x0, x0, Loop2
     Exit2: addi x18, x18, 1
