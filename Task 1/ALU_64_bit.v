117006eafb8b255c1aa9c5d6e55a6722e38e7160
m-sabihul-hasan <54771392+m-sabihul-hasan@users.noreply.github.com>
2020-06-27 00:00:10 +0500

    Update ALU_64_bit.v

--- Task 1/ALU_64_bit.v
+++ Task 1/ALU_64_bit.v
@@ -2,7 +2,6 @@ module ALU_64_bit
 (
 	input [63:0]a, [63:0]b, [3:0]ALUOp,
 	output reg Zero,
-	output reg Sign,
 	output reg [63:0]Result
 );
 
@@ -18,7 +17,6 @@ always@(a or b or ALUOp)
             4'b0110:
 			begin
 			Result = a - b;    // subtraction
-			Sign = Result[63];
 			end
 			4'b1100:
 			Result = ~(a | b);	// NOR
